13de14bb175718e55d390ea02c74cfa9a643d3ac
Ali Hassan <underlordofsouls@gmail.com>
2021-07-06 10:30:38 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, flash, redirect
+import os
+from color_extracting import extract_dominant_colors
+import pyperclip
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '//////'
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
+app.config['ALLOWED_EXTENSIONS'] = ['png', 'jpg', 'jpeg']
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    colors = extract_dominant_colors('static/uploads/default.jpg', 10)
+    if request.method == "POST":
+
+        if request.files:
+            image = request.files["image"]
+
+            try:
+                image.save(os.path.join('static/uploads/', image.filename))
+            except IsADirectoryError:
+                flash('Select an Image First!')
+                return render_template('index.html', uploaded_image='uploads/default.jpg', colors=colors)
+
+            colors = extract_dominant_colors(f'static/uploads/{image.filename}', 10)
+            return render_template('index.html', uploaded_image=f'uploads/{image.filename}', colors=colors)
+
+    # delete the files from upload folder after every revisit to home page
+    total_files = os.listdir(os.path.join('static/uploads/'))
+    for file in total_files:
+        if file != 'default.jpg':
+            os.remove(os.path.join(f'static/uploads/{file}'))
+
+    return render_template('index.html', uploaded_image='uploads/default.jpg', colors=colors)
+
+
+@app.route('/to_clipboard/<color>')
+def to_clipboard(color):
+    pyperclip.copy(f'#{color}')
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ color_extracting.py
@@ -0,0 +1,33 @@
+from PIL import Image, UnidentifiedImageError
+from colorthief import ColorThief
+import os
+
+
+def extract_dominant_colors(file, color_count):
+
+    # Open Image
+    try:
+        picture = Image.open(file)
+    except UnidentifiedImageError:
+        return 'Image Not Identified!'
+
+    # Optimize Image and save
+    img_name = f"compressed_{file.split('/')[2]}"
+
+    picture.thumbnail((200, 200))
+    picture.save(f"static/uploads/{img_name}")
+
+    # Open optimized Image and extract colors
+    color_thief = ColorThief(f"static/uploads/{img_name}")
+    # build a color palette
+    palette = color_thief.get_palette(color_count=color_count)
+
+    # delete optimized image
+    os.remove(f'static/uploads/{img_name}')
+
+    # Convert rgb colors to hex and return
+    def rgb_to_hex(rgb):
+        return '%02x%02x%02x' % rgb
+
+    return list(map(rgb_to_hex, palette))
+
